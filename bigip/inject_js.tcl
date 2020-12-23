62ffd189a67252898f1f6d52b6498a5c14ee0bee
Ubuntu <ubuntu@ip-172-31-9-47.ap-east-1.compute.internal>
2020-12-23 12:47:00 +0700

    adding bigip irule files

---
+++ bigip/inject_js.tcl
@@ -0,0 +1,19 @@
+when HTTP_REQUEST {
+   STREAM::disable
+   if { [HTTP::version] eq "1.1" } {
+      if { [HTTP::header is_keepalive] } {
+        HTTP::header replace "Connection" "Keep-Alive"
+      }
+   }
+}
+when HTTP_RESPONSE {
+   if {[HTTP::header Content-Type] starts_with "text/html"} {
+      STREAM::expression {@</head>@</head><script async defer src="https://dip.zeronaught.com/__imp_apg__/js/f5cs-a_aabz35oIkM-6b92a1c3.js" id="_imp_apg_dip_" _imp_apg_cid_="f5cs-a_aabz35oIkM-6b92a1c3" _imp_apg_api_domain_="https://dip.zeronaught.com"></script>@}
+      STREAM::enable
+   }
+}
+when STREAM_MATCHED {
+	
+   STREAM::disable
+}
+
