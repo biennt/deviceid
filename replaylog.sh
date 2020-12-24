1174f4f3148381f3e717786eb212eeebea7ff7f0
Nguyen Thanh Bien <bien@bienlab.com>
2020-12-24 15:45:50 +0700

    Update replaylog.sh

--- replaylog.sh
+++ replaylog.sh
@@ -3,7 +3,7 @@ input=$1
 while IFS= read -r line
 do
 msg=$line
-echo $msg |  sed -e 's/^[ \t]*//' |  nc -v -t  127.0.0.1 5140
+echo $msg |  sed -e 's/^[ \t]*//' |  nc -v -t  -w 1 -N 127.0.0.1 5140
 echo $msg
 done < "$input"
 
