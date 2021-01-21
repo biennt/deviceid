1fb469b6a511e0baab80af555f5067ace3c18041
Nguyen Thanh Bien <bien@bienlab.com>
2021-01-21 14:52:38 +0700

    Update inject_js.tcl

--- bigip/inject_js.tcl
+++ bigip/inject_js.tcl
@@ -8,7 +8,7 @@ when HTTP_REQUEST {
 }
 when HTTP_RESPONSE {
    if {[HTTP::header Content-Type] starts_with "text/html"} {
-      STREAM::expression {@</head>@</head><script async defer src="https://dip.zeronaught.com/__imp_apg__/js/f5cs-a_aabz35oIkM-6b92a1c3.js" id="_imp_apg_dip_" _imp_apg_cid_="f5cs-a_aabz35oIkM-6b92a1c3" _imp_apg_api_domain_="https://dip.zeronaught.com"></script>@}
+      STREAM::expression {@</head>@<script async defer src="https://dip.zeronaught.com/__imp_apg__/js/f5cs-a_aabz35oIkM-6b92a1c3.js" id="_imp_apg_dip_" _imp_apg_cid_="f5cs-a_aabz35oIkM-6b92a1c3" _imp_apg_api_domain_="https://dip.zeronaught.com"></script></head>@}
       STREAM::enable
    }
 }
